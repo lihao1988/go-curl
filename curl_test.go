4c9e598b12844ec979fb708a698c461f195f9ef5
liguangmin <li-h-8@qq.com>
2023-02-17 08:58:25 +0800

    curl test demo

--- curl_test.go
+++ curl_test.go
@@ -8,39 +8,54 @@ import (
 func TestGet(t *testing.T) {
 	client := NewClient("http://localhost:8083")
 	data := map[string]string{"namespace": "1"}
-	dataBytes, err := client.Curl("/iaas/appio?namespace=1", Get, data, JsonType)
 
-	fmt.Println(string(dataBytes), err)
+	dataBytes, err := client.Get("/iaas/appio/ab", data)
+	fmt.Println("Get: ", string(dataBytes), err)
+
+	dataBytes, err = client.Curl("/iaas/appio/ab", Get, data, JsonType)
+	fmt.Println("curl-get: ", string(dataBytes), err)
 }
 
 func TestPostJson(t *testing.T) {
 	client := NewClient("http://localhost:8083")
 	data := map[string]string{"namespace": "1"}
-	dataBytes, err := client.Curl("/iaas/appio/ab", Post, data, FormType)
 
-	fmt.Println(string(dataBytes), err)
+	dataBytes, err := client.PostByForm("/iaas/appio/ab", data)
+	fmt.Println("Post: ", string(dataBytes), err)
+
+	dataBytes, err = client.Curl("/iaas/appio/ab", Post, data, FormType)
+	fmt.Println("curl-post: ", string(dataBytes), err)
 }
 
 func TestPutJson(t *testing.T) {
 	client := NewClient("http://localhost:8083")
 	data := map[string]string{"namespace": "1"}
-	dataBytes, err := client.Curl("/iaas/appio/ab", Put, data, JsonType)
 
-	fmt.Println(string(dataBytes), err)
+	dataBytes, err := client.Put("/iaas/appio/ab", data)
+	fmt.Println("Put: ", string(dataBytes), err)
+
+	dataBytes, err = client.Curl("/iaas/appio/ab", Put, data, JsonType)
+	fmt.Println("curl-put: ", string(dataBytes), err)
 }
 
 func TestPatchJson(t *testing.T) {
 	client := NewClient("http://localhost:8083")
 	data := map[string]string{"namespace": "1"}
-	dataBytes, err := client.Curl("/iaas/appio/ab", Patch, data, FormType)
 
-	fmt.Println(string(dataBytes), err)
+	dataBytes, err := client.Patch("/iaas/appio/ab", data)
+	fmt.Println("Patch: ", string(dataBytes), err)
+
+	dataBytes, err = client.Curl("/iaas/appio/ab", Patch, data, JsonType)
+	fmt.Println("curl-patch: ", string(dataBytes), err)
 }
 
 func TestDelete(t *testing.T) {
 	client := NewClient("http://localhost:8083")
 	data := map[string]string{"namespace": "1"}
-	dataBytes, err := client.Curl("/iaas/appio", Delete, data, JsonType)
 
-	fmt.Println(string(dataBytes), err)
+	dataBytes, err := client.Curl("/iaas/appio", Delete, data, JsonType)
+	fmt.Println("Delete: ", string(dataBytes), err)
+
+	dataBytes, err = client.Curl("/iaas/appio", Delete, data, JsonType)
+	fmt.Println("curl-delete: ", string(dataBytes), err)
 }
